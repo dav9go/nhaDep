eaf676764c4e86485d4b5b0fe156d9e2f07c7bbc
David <websajang@gmail.com>
2023-02-12 17:47:56 +0700

    12 Feb 2023

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,39 +1,49 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
 export default function Contact() {
   return (
-    <div>
-      "Contact Us<br></br>
-      <br></br>
-      At [Your Company Name], we value your opinions, feedback and questions.
-      That's why we make it easy for you to get in touch with us anytime.
-      Whether you have a general inquiry or a specific request, we're here to
-      help.<br></br>
-      <br></br>
-      You can reach us through the following channels:<br></br>
-      <br></br>
-      Email: [Your email address]<br></br>
-      We check our email regularly and strive to respond to all messages within
-      24 hours. If you have a more pressing matter, please don't hesitate to
-      send us an email.<br></br>
-      <br></br>
-      WhatsApp: [Your WhatsApp number]<br></br>
-      For a quick and convenient way to get in touch, you can send us a message
-      on WhatsApp. We'll do our best to respond to your message as soon as
-      possible.<br></br>
-      <br></br>
-      Zalo: [Your Zalo number]<br></br>
-      If you prefer to use the Zalo app, we'd be happy to receive your message
-      there as well.<br></br>
-      <br></br>
-      Facebook: [Your Facebook page link]<br></br>
-      You can also connect with us on Facebook. We'll be sure to respond to your
-      message there as well.<br></br>
-      <br></br>
-      No matter how you choose to reach us, we promise to respond promptly and
-      provide the information or assistance you need.<br></br>
-      <br></br>
-      Best regards,<br></br>
-      [Your Name or Company Name]"<br></br>
-      <br></br>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-1">
+        <section className="max-w-7xl mx-auto mt-10 flex flex-col gap-5">
+          <div>
+            <h1 className="font-bold text-4xl text-center lg:text-start ">
+              Contact Us
+            </h1>
+            <br></br>
+            <br></br>
+            At NỘI THẤT NHÀ ĐẸP, we value your opinions, feedback and questions.
+            That's why we make it easy for you to get in touch with us anytime.
+            Whether you have a general inquiry or a specific request, we're here
+            to help.<br></br>
+            <br></br>
+            You can reach us through the following channels:<br></br>
+            <br></br>
+            Email: david9go@gmail.com<br></br>
+            We check our email regularly and strive to respond to all messages
+            within 24 hours. If you have a more pressing matter, please don't
+            hesitate to send us an email.<br></br>
+            <br></br>
+            WhatsApp: +34 608638374<br></br>
+            For a quick and convenient way to get in touch, you can send us a
+            message on WhatsApp. We'll do our best to respond to your message as
+            soon as possible.<br></br>
+            <br></br>
+            Zalo: +34 608638374<br></br>
+            If you prefer to use the Zalo app, we'd be happy to receive your
+            message there as well.<br></br>
+            <br></br>
+            No matter how you choose to reach us, we promise to respond promptly
+            and provide the information or assistance you need.<br></br>
+            <br></br>
+            Best regards,<br></br>
+            NỘI THẤT NHÀ ĐẸP<br></br>
+            <br></br>
+          </div>
+        </section>
+      </div>
+      <Footer />
     </div>
   );
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -9,6 +9,7 @@ import prueba2 from "../assets/prueba2.jpg";
 import prueba3 from "../assets/prueba3.jpg";
 import prueba4 from "../assets/prueba4.jpg";
 import prueba5 from "../assets/prueba5.jpg";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
@@ -120,90 +121,215 @@ export default function Home() {
     <div className="flex flex-col min-h-screen max-w-screen">
       <Navbar />
       <div className="flex-1">
-        <section className="max-w-7xl mx-auto mt-10 flex flex-col gap-5 px-5 lg:px-0 ">
-          <div>
-            <h1 className="font-bold text-4xl text-center lg:text-start ">
-              The Community Showcase
-            </h1>
-            <p className="font-montserrat mt-2 text-md max-w-7xl text-justify lg:text-start">
-              Discover the latest interior design trends and transform your home
-              into a beautiful oasis with the help of our online interior house
-              design platform. Browse thousands of inspiring home decor ideas,
-              from modern to traditional, and everything in between. Get
-              inspired by a vast library of customizable furniture, materials,
-              and color palettes. Collaborate with our professional interior
-              designers and receive tailored design plans and shopping lists.
-              Create the home of your dreams with our easy-to-use, user-friendly
-              platform, available 24/7. Start designing your perfect interior
-              space now.
-            </p>
-          </div>
-        </section>
-        <div className="max-w-7xl mx-auto mt-10 flex flex-col gap-5">
-          <h2 className="text-3xl text-center">
-            Top 5 posts of the last 30 days
-          </h2>
-        </div>
-        <section className=" flex w-full h-[430px] mt-10">
-          <img
-            className="w-[0px] grow object-cover opacity-80 duration-500 ease-in hover:w-[300px] hover:opacity-100 hover:contrast-125 hover:cursor-pointer"
-            src={topFiveLikedPostsLastMonth[3]?.photo}
-          />
-          <img
-            className="w-[0px] grow object-cover opacity-80 duration-500 ease-in hover:w-[300px] hover:opacity-100 hover:contrast-125 hover:cursor-pointer"
-            src={topFiveLikedPostsLastMonth[1]?.photo}
-          />
-          <img
-            className="w-[0px] grow object-cover opacity-80 duration-500 ease-in hover:w-[300px] hover:opacity-100 hover:contrast-125 hover:cursor-pointer"
-            src={topFiveLikedPostsLastMonth[0]?.photo}
-          />
-          <img
-            className="w-[0px] grow object-cover opacity-80 duration-500 ease-in hover:w-[300px] hover:opacity-100 hover:contrast-125 hover:cursor-pointer"
-            src={topFiveLikedPostsLastMonth[2]?.photo}
-          />
-          <img
-            className="w-[0px] grow object-cover opacity-80 duration-500 ease-in hover:w-[300px] hover:opacity-100 hover:contrast-125 hover:cursor-pointer"
-            src={topFiveLikedPostsLastMonth[4]?.photo}
-          />
-        </section>
-        <section className="max-w-7xl mx-auto mt-10 flex flex-col gap-5">
-          <div className="px-5 lg:px-0">
-            <FormField
-              labelName="Search posts"
-              type="text"
-              name="text"
-              placeholder="Search posts"
-              value={searchText}
-              handleChange={handleSearchChange}
-            />
-          </div>
-
-          <div className="mt-10">
-            {loading ? (
-              <div className="flex justify-center items-center">
-                <Loader />
+        {allPosts && (
+          <>
+            <section className="max-w-7xl mx-auto mt-10 flex flex-col gap-5 px-5 lg:px-0 ">
+              <div>
+                <h1 className="font-bold text-4xl text-center lg:text-start ">
+                  The Community Showcase
+                </h1>
+                <p className="font-montserrat mt-2 text-md max-w-7xl text-justify lg:text-start">
+                  Discover the latest interior design trends and transform your
+                  home into a beautiful oasis with the help of our online
+                  interior house design platform. Browse thousands of inspiring
+                  home decor ideas, from modern to traditional, and everything
+                  in between. Get inspired by a vast library of customizable
+                  furniture, materials, and color palettes. Collaborate with our
+                  professional interior designers and receive tailored design
+                  plans and shopping lists. Create the home of your dreams with
+                  our easy-to-use, user-friendly platform, available 24/7. Start
+                  designing your perfect interior space now.
+                </p>
               </div>
-            ) : (
-              <>
-                <div className="grid md:grid-cols-2 grid-cols-1 gap-3">
-                  {searchText ? (
-                    <RenderCards
-                      data={searchedResults}
-                      title="No search results found"
-                    />
-                  ) : (
-                    <RenderCards
-                      allPosts={allPosts}
-                      data={allPosts}
-                      setAllPosts={setAllPosts}
-                      title="No posts found"
-                    />
-                  )}
-                </div>
-              </>
-            )}
-          </div>
-        </section>
+            </section>
+            <div className="max-w-7xl mx-auto mt-10 flex flex-col gap-5">
+              <h2 className="text-3xl text-center">
+                Top 5 posts of the last 30 days
+              </h2>
+            </div>
+            <section className="relative flex w-full h-[430px] mt-10">
+              <div className="relative w-[0px] group grow object-cover opacity-80 duration-500 ease-in hover:w-[300px] hover:opacity-100 hover:contrast-125 hover:cursor-pointer hover:text-white">
+                <Link to={`/post/${topFiveLikedPostsLastMonth[3]?._id}`}>
+                  <img
+                    className="h-full w-full grow object-cover"
+                    src={topFiveLikedPostsLastMonth[3]?.photo}
+                  />
+                  <div className="absolute rounded-md bottom-0 mx-auto m-5 p-10 w-9/12 bg-black/40 group-hover:bg-black/75 hidden group-hover:flex justify-around">
+                    <div>{topFiveLikedPostsLastMonth[3]?.user}</div>
+                    <div className="flex items-center gap-2">
+                      {topFiveLikedPostsLastMonth[3]?.likes.length}{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+
+              <div className="relative w-[0px] group grow object-cover opacity-80 duration-500 ease-in hover:w-[300px] hover:opacity-100 hover:contrast-125 hover:cursor-pointer hover:text-white">
+                <Link to={`/post/${topFiveLikedPostsLastMonth[1]?._id}`}>
+                  <img
+                    className="h-full w-full grow object-cover"
+                    src={topFiveLikedPostsLastMonth[1]?.photo}
+                  />
+                  <div className="absolute rounded-md bottom-0 mx-auto m-5 p-10 w-9/12 bg-black/40 group-hover:bg-black/75 hidden group-hover:flex justify-around">
+                    <div>{topFiveLikedPostsLastMonth[1]?.user}</div>
+                    <div className="flex items-center gap-2">
+                      {topFiveLikedPostsLastMonth[1]?.likes.length}{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+              <div className="relative w-[0px] group grow object-cover opacity-80 duration-500 ease-in hover:w-[300px] hover:opacity-100 hover:contrast-125 hover:cursor-pointer hover:text-white">
+                <Link to={`/post/${topFiveLikedPostsLastMonth[0]?._id}`}>
+                  <img
+                    className="h-full w-full grow object-cover"
+                    src={topFiveLikedPostsLastMonth[0]?.photo}
+                  />
+                  <div className="absolute rounded-md bottom-0 mx-auto m-5 p-10 w-9/12 bg-black/40 group-hover:bg-black/75 hidden group-hover:flex justify-around">
+                    <div>{topFiveLikedPostsLastMonth[0]?.user}</div>
+                    <div className="flex items-center gap-2">
+                      {topFiveLikedPostsLastMonth[0]?.likes.length}{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+              <div className="relative w-[0px] group grow object-cover opacity-80 duration-500 ease-in hover:w-[300px] hover:opacity-100 hover:contrast-125 hover:cursor-pointer hover:text-white">
+                <Link to={`/post/${topFiveLikedPostsLastMonth[2]?._id}`}>
+                  <img
+                    className="h-full w-full grow object-cover"
+                    src={topFiveLikedPostsLastMonth[2]?.photo}
+                  />
+                  <div className="absolute rounded-md bottom-0 mx-auto m-5 p-10 w-9/12 bg-black/40 group-hover:bg-black/75 hidden group-hover:flex justify-around">
+                    <div>{topFiveLikedPostsLastMonth[2]?.user}</div>
+                    <div className="flex items-center gap-2">
+                      {topFiveLikedPostsLastMonth[2]?.likes.length}{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+              <div className="relative w-[0px] group grow object-cover opacity-80 duration-500 ease-in hover:w-[300px] hover:opacity-100 hover:contrast-125 hover:cursor-pointer hover:text-white">
+                <Link to={`/post/${topFiveLikedPostsLastMonth[4]?._id}`}>
+                  <img
+                    className="h-full w-full grow object-cover"
+                    src={topFiveLikedPostsLastMonth[4]?.photo}
+                  />
+                  <div className="absolute rounded-md bottom-0 mx-auto m-5 p-10 w-9/12 bg-black/40 group-hover:bg-black/75 hidden group-hover:flex justify-around">
+                    <div>{topFiveLikedPostsLastMonth[4]?.user}</div>
+                    <div className="flex items-center gap-2">
+                      {topFiveLikedPostsLastMonth[4]?.likes.length}{" "}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </section>
+            <section className="max-w-7xl mx-auto mt-10 flex flex-col gap-5">
+              <div className="px-5 lg:px-0">
+                <FormField
+                  labelName="Search posts"
+                  type="text"
+                  name="text"
+                  placeholder="Search posts"
+                  value={searchText}
+                  handleChange={handleSearchChange}
+                />
+              </div>
+
+              <div className="mt-10">
+                {loading ? (
+                  <div className="flex justify-center items-center">
+                    <Loader />
+                  </div>
+                ) : (
+                  <>
+                    <div className="grid md:grid-cols-2 grid-cols-1 gap-3">
+                      {searchText ? (
+                        <RenderCards
+                          data={searchedResults}
+                          title="No search results found"
+                        />
+                      ) : (
+                        <RenderCards
+                          allPosts={allPosts}
+                          data={allPosts}
+                          setAllPosts={setAllPosts}
+                          title="No posts found"
+                        />
+                      )}
+                    </div>
+                  </>
+                )}
+              </div>
+            </section>
+          </>
+        )}
       </div>
       <Footer />
     </div>
